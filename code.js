7ec562e14c2e1a8e01aeef1c5cfe5a0236537a75
jaredGuzman <jarededwardguzman25@gmail.com>
2021-12-29 12:10:02 -0800

    Added more UI & hoverColor, hoverParty, and hoverOpacity functions

--- code.js
+++ code.js
@@ -1,84 +1,91 @@
-let defaultSize = 16,
-    currentSize = 16;
-let currentHoverColor = "#323875";
+let currentSize = 16,
+    currentColor = "#323875",
+    opacityValues = [],
+    currentMode = "color";
+
 let slider = document.querySelector('.range-slider'),
     sliderValue = document.querySelector('.range-slider-value').value,
     sliderValueOutput = document.querySelector('.range-slider-value');
-
-
-
-    //
-    //  ONCE THIS IS DONE, REGENERATE GRID USING NEW VALUE
-    //      -- update currentsize, add event listener to input on 'mouseup' maybe?
-    //
     slider.oninput = function () {sliderValueOutput.textContent = this.value;};
-    
-    
 
-
-
-//
-//      NEED TO CHANGE TO UPDATE USING USER INPUT
-//
 function hoverColor(item, color){
     item.addEventListener("mouseenter", () => {
-        // highlight the mouseenter target
         item.style.background = color;
       }, false);
 }
 
-// CAN MAYBE BE REFACTORED TO BE SIMPLER - this plus generaterow both inside the generate grid function maybe?
-function generateDiv() {
+function hoverParty(item){
+    let randomColor = Math.floor(Math.random()*0xFFFFFF<<0).toString(16);
+    item.addEventListener("mouseenter", () => {
+        item.style.background = `#${randomColor}`;
+      }, false);
+}
+
+function hoverOpacity(item, indexNum){
+    opacityValues.push(0);
+    item.classList.add(`item-${indexNum}`);
+    item.style.opacity = 0;
+    item.addEventListener("mouseenter", () => {
+        let thisIndex = parseFloat(item.className.slice(5)) - 1;
+        let updatedOpacity = opacityValues[thisIndex] + 1;
+        opacityValues[thisIndex] <= 10 
+            ? opacityValues[thisIndex] = updatedOpacity 
+            : updatedOpacity = 10;
+        item.style.background = currentColor;      
+        item.style.opacity = updatedOpacity / 10;
+      }, false);
+}
+
+function generateDiv(columnPos, rowNum, size) {
     let item = document.createElement("div");
-        hoverColor(item, currentHoverColor);
+    let indexNum = (((rowNum - 1) * size) + columnPos);
+    // Cases
+    switch(currentMode){
+        case 'color':
+            hoverColor(item, currentColor);
+            break;
+        case 'party':
+            hoverParty(item);
+            break;
+        case 'opacity':
+            hoverOpacity(item, indexNum);
+        default:
+            hoverColor(item, currentColor);
+    } 
     return item
 }
 
-function generateRow(size){
+function generateRow(size, rowNum){
     let row = document.createElement("div");
         for( let columnPos = 1; columnPos <= size; columnPos++){
-            let generatedDiv = generateDiv(columnPos);
-                row.appendChild(generatedDiv);
+                row.appendChild(generateDiv(columnPos, rowNum, size));
         }
     return row
 }
 
-function deleteGrid(currentGrid){
-    // 
-    //      DELETE ALL ELEMENT IN GRID AND GRID ELEMENT TOO
-    //
-    //
-    //
-    currentGrid.forE
-}
-
 function generateGrid(size){
-    let currentGrid = document.querySelector(".grid");
-    console.log(!!currentGrid);
+    let currentGrid = document.querySelector(".grid"),
+        grid = document.createElement("div");
+        grid.classList.add("grid");
+
     if(!!currentGrid){
-        deleteGrid(currentGrid);
+        currentGrid.remove()
     }
-        let grid = document.createElement("div");
-        grid.classList.add(`grid`);
+
     for(let rowNum = 1; rowNum <= size; rowNum++){
-        let generatedRow = generateRow(size);
-            grid.appendChild(generatedRow);
+            grid.appendChild(generateRow(size, rowNum));
     }
     return grid
 }
 
-
-//
-//  ADD THIS FUNCTION TO ANY TIME THE USER FINISHES INPUTTING SOMETHING
-//
-function clearGrid() {
-    generateGrid(currentSize);
-}
 
 
 let button = document.querySelector("#generate-grid");
     button.addEventListener("click", () =>{
         clearGrid();
     });
+
+
+// Initializing the grid
 let root = document.querySelector(".root");
     root.appendChild(generateGrid(16, 16));
